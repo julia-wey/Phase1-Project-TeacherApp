61b06a9c91076e78bf84791c9d550a1600c99e78
julia-wey <135829299+julia-wey@users.noreply.github.com>
2024-04-12 14:27:03 -0400

    Add files via upload

---
+++ Project/index.js
@@ -0,0 +1,14 @@
+console.log("connected")
+
+//fetch here
+fetch
+//here- forEach loop with mouse event to display criteria
+/*
+1- function displayCriteria
+2- event mouseover
+3- foreach- displaystrands
+4- 
+*/
+
+
+//document.querySelector("#box1")
